718a794b1307e412de85719678e193b45c59257b
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-05-31 19:52:42 -0500

    add some style

---
+++ src/components/Category.tsx
@@ -0,0 +1,7 @@
+import {} from 'react-icons'
+
+export const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
--- src/components/Veggie.tsx
+++ src/components/Veggie.tsx
@@ -1,6 +1,97 @@
+import { useEffect, useId, useState } from "react";
+import { Recipe } from "../interfaces/interfaces";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/css";
 
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 24rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 export const Veggie = () => {
+
+    const [veggie, setVeggie] = useState([]);
+
+  const getPopular = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?number=10&tags=vegetarian&apiKey=${
+          import.meta.env.VITE_TEST_VAR
+        }`
+      );
+      const data = await api.json();
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+      console.log("EJECTUANDO");
+    }
+  };
+  useEffect(() => {
+    getPopular();
+  }, []);
   return (
-    <div>Veggie</div>
+    <Wrapper>
+    <h3>Our Vegetarian Picks</h3>
+    <Splide
+      options={{
+        perPage: 3,
+        arrows: false,
+        pagination: false,
+        drag: "free",
+        gap: "5rem",
+      }}
+    >
+      {veggie?.map((recipe: Recipe, i: number) => (
+        <SplideSlide>
+          <Card key={recipe.id * i}>
+            <p>{recipe.title}</p>
+            <img src={recipe.image} alt={recipe.title} />
+            <Gradient />
+          </Card>
+        </SplideSlide>
+      ))}
+    </Splide>
+  </Wrapper>
   )
 }
